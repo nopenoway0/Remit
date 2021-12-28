78ba48315ef8beaa3bd99ede0c348026c372e098
Ben <kite670@yahoo.com>
2021-12-28 00:04:01 -0800

    added processing for file add and modification info

--- src-tauri/src/fileeventconsumer/mod.rs
+++ src-tauri/src/fileeventconsumer/mod.rs
@@ -1,5 +1,8 @@
 pub mod rustssh {
     use windows::Win32::Storage::FileSystem::*;
+    use crate::syncmanager::rustssh::RCloneManager;
+    use crate::systempaths::rustssh::SystemPath;
+    use crate::sessionmanager::rustssh::Directory;
     use std::sync::{Arc, Mutex};
     use std::thread::*;
     #[derive(Eq, PartialEq)]
@@ -22,24 +25,91 @@ pub mod rustssh {
 
     pub struct FileEventConsumer {
         consumable: Arc::<Mutex::<Vec::<FileEvent>>>,
-        thread_status: Arc::<Mutex::<ThreadStatus>>
+        thread_status: Arc::<Mutex::<ThreadStatus>>,
+        batch_size: Arc::<u32>,
+        rclone_instance: Arc::<Mutex::<RCloneManager>>
+    }
+
+    fn process_event(instance: &Arc::<Mutex::<RCloneManager>>, event: &FileEvent) {
+        println!("processing event for {}", event.file_name);
+        match event.event_type {
+            FILE_ACTION_ADDED => {
+                let mut dir = Directory::new(None);
+                dir.path = SystemPath::new();
+                dir.path.set_win_path(format!("\\{}", event.file_name.clone()));
+                let filename = dir.path.popd();
+                let r = instance.lock().unwrap().upload_local_file(&mut dir, filename);
+                if r.is_err() {
+                    println!("Error uploading file");
+                }
+            },
+            FILE_ACTION_MODIFIED => {
+                let mut dir = Directory::new(None);
+                dir.path = SystemPath::new();
+                dir.path.set_win_path(format!("\\{}", event.file_name.clone()));
+                let filename = dir.path.popd();
+                let r = instance.lock().unwrap().upload_local_file(&mut dir, filename);
+                if r.is_err() {
+                    println!("Error uploading file");
+                }  
+            },
+            _ => {
+
+            }
+        }
     }
 
     impl FileEventConsumer {
-        pub fn start(&mut self) {
-            *self.thread_status.lock().unwrap() = ThreadStatus::Resume;
-            let consumable_shared = self.consumable.clone();
-            let shared_status = self.thread_status.clone();
+
+        pub fn get_data_reference(&mut self) -> Arc::<Mutex::<Vec::<FileEvent>>> {
+            return self.consumable.clone();
+        }
+
+        pub fn new(rclone_manager: Arc::<Mutex::<RCloneManager>>) -> FileEventConsumer{
+            let consumer = FileEventConsumer{consumable: Arc::new(Mutex::new(Vec::new())),
+                                            thread_status: Arc::new(Mutex::new(ThreadStatus::Pause)),
+                                            batch_size: Arc::new(5u32),
+                                            rclone_instance: rclone_manager.clone()};
+            let consumable_shared = consumer.consumable.clone();
+            let shared_status = consumer.thread_status.clone();
+            let shared_rclone_m = consumer.rclone_instance.clone();
             spawn(move || {
                 let sleep_time = std::time::Duration::from_millis(1000);
-                while *shared_status.lock().unwrap() == ThreadStatus::Resume {
+                while *shared_status.lock().unwrap() != ThreadStatus::Kill  {
                     std::thread::sleep(sleep_time);
+                    if *shared_status.lock().unwrap() == ThreadStatus::Pause {
+                        continue;
+                    }
+                    let mut local_data = Vec::<FileEvent>::new();
+                    {
+                        let mut shared_data = consumable_shared.lock().unwrap();
+                        let mut loop_size = 5;
+                        if loop_size > shared_data.len() {
+                            loop_size = shared_data.len();
+                        }
+                        for _ in 0..loop_size {
+                            local_data.push(shared_data.pop().unwrap());
+                        }
+                    }
+                    for x in &local_data {
+                        process_event(&shared_rclone_m, x);
+                    }
                 }
+                println!("ending file event conumser");
             });
+            return consumer;
+        }
+
+        pub fn start(&mut self) {
+            *self.thread_status.lock().unwrap() = ThreadStatus::Resume;
         }
 
         pub fn add_event(&mut self, f: FileEvent) {
             self.consumable.lock().unwrap().push(f);
+        }
+
+        pub fn clear(&mut self) {
+            self.consumable.lock().unwrap().clear();
         }
 
         pub fn pause(&mut self) {
