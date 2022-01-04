05aba2595763f389405cbfef3cf4a52041957dad
Ben <kite670@yahoo.com>
2022-01-04 11:14:21 -0800

    moved methods to class, removed unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Component } from 'react/cjs/react.production.min'
-import { Backdrop, CircularProgress } from '@mui/material'
-import OkDialog from './OkDialog.js'
 import Login from './Login.js'
 import Navigator from './Navigator'
-import { invoke } from '@tauri-apps/api/tauri';
 import './App.css'
-
+/**
+ * Application class
+ */
 class App extends Component{
 
   constructor(props) {
@@ -17,20 +16,25 @@ class App extends Component{
                   startup: true,
                   configs: {}};
   }
+  /**
+   * set logged in state to true
+   */
+  login() {
+    this.setState({logged: true});
+  }
+
+  /**
+   * set logged in state to false
+   */
+  logout() {
+    this.setState({logged: false});
+  }
 
   render() {
 
-    const logged_in = () => {
-      this.setState({logged: true});
-    };
-
-    const logged_out = () => {
-      this.setState({logged: false});
-    }
-
-    let page = <Login loggedInCallback={logged_in.bind(this)}/>
+    let page = <Login loggedInCallback={this.login.bind(this)}/>
     if (this.state.logged)
-      page = <Navigator disconnectHandler={logged_out.bind(this)}/>;
+      page = <Navigator disconnectHandler={this.logout.bind(this)}/>;
     return (<div>
               {page}
             </div> );
