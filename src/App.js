6210dbf32b534689fa80b6a5fefc842700dd9c8c
Ben <kite670@yahoo.com>
2022-01-04 23:36:49 -0800

    SaveManager call added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Component } from 'react/cjs/react.production.min'
 import Login from './Login.js'
 import Navigator from './Navigator'
+import SaveManager from './SaveManager'
 import './App.css'
 /**
  * Application class
@@ -14,7 +15,9 @@ class App extends Component{
                   disableScreen: true,
                   missingDeps: false,
                   startup: true,
-                  configs: {}};
+                  configs: {},
+                  openSaveManager: false,
+                  saveManagerData: undefined};
   }
   /**
    * set logged in state to true
@@ -30,11 +33,24 @@ class App extends Component{
     this.setState({logged: false});
   }
 
+  openSaveManager(data) {
+    console.log("open save manager");
+    this.setState({saveManagerData: data});
+  }
+
+  closeSaveManager() {
+    this.setState({saveManagerData: undefined});
+  }
+
   render() {
 
-    let page = <Login loggedInCallback={this.login.bind(this)}/>
-    if (this.state.logged)
+    let page = <Login openSaveManagerHandler={this.openSaveManager.bind(this)} loggedInCallback={this.login.bind(this)}/>
+    if (this.state.saveManagerData) {
+      page = <SaveManager pass={this.state.saveManagerData.password} user={this.state.saveManagerData.username} 
+        host={this.state.saveManagerData.host} port={this.state.saveManagerData.port}/>
+    } else if (this.state.logged) {
       page = <Navigator disconnectHandler={this.logout.bind(this)}/>;
+    }
     return (<div>
               {page}
             </div> );
