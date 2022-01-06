38389be94f64528f6937d073e2f7b523d52d215d
Ben <kite670@yahoo.com>
2022-01-05 21:25:08 -0800

    fixed formatting, added heading, completed save function with call to backend

--- src/SaveManager.js
+++ src/SaveManager.js
@@ -1,29 +1,37 @@
 import * as React from 'react'
 import { Component } from 'react/cjs/react.production.min'
-import {Stack, TextField, Box, Backdrop, CircularProgress, Button} from '@mui/material'
-import "./App.css"
+import {Stack, TextField, Box, Button, Typography} from '@mui/material'
 import ButtonLoader from './ButtonLoader';
 import OkDialog from './OkDialog'
+import { invoke } from '@tauri-apps/api/tauri';
+import "./App.css"
+
 class SaveMananger extends Component {
 
     constructor(props) {
         super(props);
         this.state = {showDialog: false,
-                        dialogText: ""};
+                        dialogText: "",
+                        showPassword: false,
+                        dialogClickHandler: ()=>{}};
+    }
+
+    getFormData() {
+        let fields = {};
+        fields.host = document.getElementById("host").value;
+        fields.user = document.getElementById("username").value;
+        fields.port = document.getElementById("port").value;
+        fields.password = document.getElementById("password").value; 
+        fields.name = document.getElementById("name").value;
+        return fields;
     }
 
     //TODO rewrite a better for loop
     getIncorrectInputs() {
-        let fields = {};
-        fields.host = document.getElementById("host").value;
-        fields.username = document.getElementById("username").value;
-        fields.port = document.getElementById("port").value;
-        fields.password = document.getElementById("password").value; 
-        fields.name = document.getElementById("name").value;
-
+        let fields = this.getFormData();
         let error_fields = {errors:0};
-        Object.entries(fields).forEach((key) => {
-            if (fields[key] == undefined || fields[key].len == 0) {
+        Object.entries(fields).forEach((entry) => {
+            if (entry[1] == undefined || entry[1].length == 0) {
                 error_fields.errors += 1;
             }
         });
@@ -37,36 +45,50 @@ class SaveMananger extends Component {
             if (errors.errors > 0) {
                 rej(JSON.stringify(errors));
             } else {
-                res("Saved");
+                invoke("plugin:Remit|save_config", this.getFormData())
+                    .then((e)=>res(e))
+                    .catch((e)=>rej(e));
             }
         });
     }
 
     success(s) {
-        this.setState({showDialog: true, dialogText: s});
+        this.setState({showDialog: true, dialogText: s, dialogClickHandler:this.closeHandler.bind(this)});
     }
 
     fail(f) {
-        this.setState({showDialog: true, dialogText: f});
+        this.setState({showDialog: true, dialogText: f, dialogClickHandler:()=>this.setState({showDialog: false})});
+    }
+
+    closeHandler() {
+        this.props.onClose();
     }
 
     render() {
+        let name = (this.props.name != undefined) ? this.props.name : "";
         return (
             <div className="App">
                 <body className="App-header">
-                    <OkDialog show={this.state.showDialog} text={this.state.dialogText} title={"Save Status"} onClick={()=>this.setState({showDialog: false})} />
+                    <OkDialog show={this.state.showDialog} text={this.state.dialogText} title={"Save Status"} onClick={this.state.dialogClickHandler.bind(this)} />
                     <Box sx={{bgcolor: 'background.paper', overflow:'hidden', borderRadius:'12px', boxShadow: 1, display: 'flex',
                             flexDirection:{ xs: 'column', md: 'row'}}}>
                         <Stack sx={{margin:4 }}>
-                            <TextField key="username" variant="standard" required label="Username" value={this.props.user} id="username" ></TextField>
-                            <TextField key="password" variant="standard" required label="Password" type="password" id="password" value={this.props.pass}></TextField>
-                            <TextField key="host" variant="standard" required label="Host" id="host" value={this.props.host}/>
-                            <TextField key="port" variant="standard" required label="Port" id="port" value={this.props.port}/>
-                            <TextField key="name" variant="standard" required label="Name" id="name"/>
+                            <Typography sx={{color:'black'}} variant="h6" gutterBottom>
+                                Enter Configuration Information
+                            </Typography>
+                            <TextField key="username" variant="standard" required label="Username" defaultValue={this.props.user} id="username"></TextField>
+                            <TextField key="password" variant="standard" required label="Password" type="password" id="password" defaultValue={this.props.pass}></TextField>
+                            <TextField key="host" variant="standard" required label="Host" id="host" defaultValue={this.props.host}/>
+                            <TextField key="port" variant="standard" required label="Port" id="port" defaultValue={this.props.port}/>
+                            <TextField key="name" variant="standard" required label="Name" id="name" defaultValue={name}/>
                         </Stack>
+                        <Box>
+                            <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+                                <Button variant="outlined" onClick={this.closeHandler.bind(this)}>Cancel</Button>
+                                <ButtonLoader text={"Save"} onClick={this.save.bind(this)} handleSuccess={this.success.bind(this)}  handleError={this.fail.bind(this)} /> 
+                            </Stack>
+                        </Box>
                     </Box>
-                    <Button>Cancel</Button>
-                    <ButtonLoader text={"Save"} onClick={this.save.bind(this)} handleSuccess={this.success.bind(this)}  handleError={this.fail.bind(this)} />
                 </body>
             </div>
         );
