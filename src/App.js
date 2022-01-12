84b8e5816bb808c0f34f1a1e7446c7aa005b3f7e
Ben <kite670@yahoo.com>
2022-01-12 00:43:57 -0800

    added visuals and blocking for when rclone doesn't exist

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from './Login.js'
 import Navigator from './Navigator'
 import SaveManager from './SaveManager'
 import './App.css'
+import { invoke } from '@tauri-apps/api/tauri'
+import OkDialog from './OkDialog.js'
 /**
  * Application class
  */
@@ -17,7 +19,8 @@ class App extends Component{
                   startup: true,
                   configs: {},
                   openSaveManager: false,
-                  saveManagerData: undefined};
+                  saveManagerData: undefined,
+                  rclone_exists: true};
   }
   /**
    * set logged in state to true
@@ -38,6 +41,18 @@ class App extends Component{
     this.setState({saveManagerData: data});
   }
 
+  componentDidMount() {
+    this.checkForRclone()
+      .then((r) => {
+        console.log(r);
+        this.setState({rclone_exists: r});
+      });
+  }
+
+  checkForRclone() {
+    return invoke("plugin:Remit|rclone_exe_exists");
+  }
+
   closeSaveManager() {
     this.setState({saveManagerData: undefined});
   }
@@ -45,7 +60,10 @@ class App extends Component{
   render() {
 
     let page = <Login openSaveManagerHandler={this.openSaveManager.bind(this)} loggedInCallback={this.login.bind(this)}/>
-    if (this.state.saveManagerData) {
+    if (!this.state.rclone_exists) {
+      page = <OkDialog show={true} title="rclone not found!" text="rclone-x86_64-pc-windows-msvc.exe not found. Please download rclone, rename it accordingly and place it in the directory"
+                onClick={()=>this.setState({logged:false})}/>;
+    } else if (this.state.saveManagerData) {
       page = <SaveManager pass={this.state.saveManagerData.password} user={this.state.saveManagerData.username} 
         host={this.state.saveManagerData.host} port={this.state.saveManagerData.port} onClose={this.closeSaveManager.bind(this)}/>
     } else if (this.state.logged) {
