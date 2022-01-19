ec751af7f379355135abbd5362e0d02b5d6d906e
Ben <kite670@yahoo.com>
2022-01-18 21:40:18 -0800

    added git remove recursive option to file deletion to work with directories

--- src-tauri/src/manager/mod.rs
+++ src-tauri/src/manager/mod.rs
@@ -4,7 +4,7 @@ pub mod rustssh{
 use std::process::Command;
 use std::env::current_dir;
 use std::sync::{Arc, Mutex};
-use std::fs::{remove_file, rename};
+use std::fs::{remove_file, rename, remove_dir_all, create_dir_all};
 use crate::*;
 
 /// Primary manager over the ssh, rclone and config managers. 
@@ -46,6 +46,14 @@ impl Manager {
         return Ok(m);
     }
 
+    pub fn create_dir(&mut self, dirname: &String) -> Result<(), IOError> {
+        let mut remote_path = self.dir.path.clone();
+        remote_path.pushd(dirname.clone());
+        self.ssh_m.run_command(format!("mkdir \"{}\"", remote_path.get_path()))?;
+        create_dir_all(format!("{}\\.remote\\{}", self.rclone_m.lock().unwrap().chosen_config.clone(), remote_path.get_windows_path_local()))?;
+        return Ok(());
+    }
+
     pub fn create_file(&mut self, filename: &String) -> Result<(), IOError>{
         let mut remote_path = Remit::SystemPath::new();
         remote_path.set_win_path(self.dir.path.get_path());
@@ -79,7 +87,7 @@ impl Manager {
 
     /// delete a file both remotely and locally. Only uses remove_file and rm ( no -r ) so doesn't work for directories
     /// Will need a more robust version to prevent accidental removal of entire directory trees
-    pub fn delete_file(&mut self, file: String) -> Result<String, IOError>{
+    pub fn delete_file(&mut self, file: String, recursive: bool) -> Result<String, IOError>{
         let mut local_path = Remit::SystemPath::new();
         local_path.set_win_path(format!("{}\\.remote\\{}", self.rclone_m.lock().unwrap().chosen_config.clone(), self.dir.path.get_windows_path_local()));
         let mut remote_path = Remit::SystemPath::new();
@@ -89,14 +97,24 @@ impl Manager {
 
         let mut result_string: String = "".to_string();
         // delete locally
-        let res1 = remove_file(local_path.get_windows_path_local());
+        let res1;
+        if recursive {
+            res1 = remove_dir_all(local_path.get_windows_path_local());
+        } else {
+            res1 = remove_file(local_path.get_windows_path_local());
+        }
         if res1.is_err() {
             result_string += &res1.unwrap_err().to_string();
         }
         println!("{}",local_path.get_windows_path_local());
         // delete remotely
         println!("rm \"{}\"", remote_path.get_path());
-        let res2 = self.ssh_m.run_command(format!("rm \"{}\"", remote_path.get_path()));
+        let res2;
+        if recursive {
+            res2 = self.ssh_m.run_command(format!("rm -r \"{}\"", remote_path.get_path()));
+        } else {
+            res2 = self.ssh_m.run_command(format!("rm \"{}\"", remote_path.get_path()));
+        }
         if res2.is_err() {
             result_string += &res2.unwrap_err().to_string();
         } else {
